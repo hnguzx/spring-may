94dcebc28d15dbbd70f41e2bab0a6165a9e8e943
hnguzx <hnguzx@qq.com>
2022-05-10 19:28:15 +0800

    排查问题，临时提交

--- demo/src/main/java/pers/guzx/demo/DemoApplication.java
+++ demo/src/main/java/pers/guzx/demo/DemoApplication.java
@@ -1,6 +1,7 @@
 package pers.guzx.demo;
 
 //import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
