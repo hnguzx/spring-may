554c5b65813b1795f4ee23fbf0ab6364ba9267cd
hnguzx <hnguzx@qq.com>
2022-05-13 17:30:28 +0800

    日志配置

--- demo/src/main/java/pers/guzx/demo/controller/Log.java
+++ demo/src/main/java/pers/guzx/demo/controller/Log.java
@@ -1,5 +1,7 @@
 package pers.guzx.demo.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -8,6 +10,17 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2021/5/15 17:47
  * @describe
  */
+@Slf4j
 @RestController
 public class Log {
+
+    @GetMapping("/log")
+    public void log(){
+        log.trace("Connect Execute start");
+        log.debug("debug log");
+        log.info("info log");
+        log.warn("warn log");
+        log.error("error log");
+    }
+
 }
