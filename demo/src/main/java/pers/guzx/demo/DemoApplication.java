2c27a59853efd45517e878b38deae0ba3541f880
hnguzx <hnguzx@qq.com>
2022-05-18 19:32:17 +0800

    添加eureka注册中心

--- demo/src/main/java/pers/guzx/demo/DemoApplication.java
+++ demo/src/main/java/pers/guzx/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package pers.guzx.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @MapperScan("pers.guzx.demo.mapper")
 @SpringBootApplication(scanBasePackages = {"pers.guzx"})
 public class DemoApplication {
