30ebffd50b532339b540277b90357fa184488d5c
hnguzx <hnguzx@qq.com>
2022-05-20 14:03:38 +0800

    降低swagger版本

--- common/src/main/java/pers/guzx/common/config/SwaggerConfig.java
+++ common/src/main/java/pers/guzx/common/config/SwaggerConfig.java
@@ -4,13 +4,13 @@ import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
  * @author Guzx
@@ -18,7 +18,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @date 2021/5/15 17:47
  * @describe
  */
-@EnableOpenApi
+@EnableSwagger2WebMvc
 @Configuration
 public class SwaggerConfig {
 
@@ -28,6 +28,7 @@ public class SwaggerConfig {
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
+                .paths(PathSelectors.any())
                 .build();
     }
 
