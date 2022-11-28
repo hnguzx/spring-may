a287faf4e988f7b8aab02f390565cfcbb42f576d
guzhixiong <guzx1@yusys.com.cn>
2022-11-28 19:12:25 +0800

    添加swagger

--- demo/consumer/src/main/java/pers/guzx/consumer/controller/CallSelfController.java
+++ demo/consumer/src/main/java/pers/guzx/consumer/controller/CallSelfController.java
@@ -1,6 +1,7 @@
 package pers.guzx.consumer.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pers.guzx.entity.Result;
 
@@ -9,7 +10,17 @@ public class CallSelfController {
 
     @GetMapping("/call/self")
     public Result<String> call() {
-        int i = 1 / 0;
+//        int i = 1 / 0;
         return Result.succeed();
+    }
+
+    @GetMapping("/call/params1")
+    public Result<String> callParameter1(@RequestParam("key") String name) {
+        return Result.succeed(name);
+    }
+
+    @GetMapping("/call/params2")
+    public Result<String> callParameter2(@RequestParam("key") String name, @RequestParam("value") String value) {
+        return Result.succeed(name + value);
     }
 }
